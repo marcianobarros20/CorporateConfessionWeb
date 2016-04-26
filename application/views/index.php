ff31ad87f48429c67fa50efbea38f9a54b045fa2
kingsuk <kingsuk.majumder@gmail.com>
2016-04-26 19:55:06 +0530

    added ico file

--- application/views/index.php
+++ application/views/index.php
@@ -8,10 +8,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Oleose App Landing Page | Bootstrap Theme</title>
+    <title>Corporate Confessions</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <link rel="shortcut icon" href="favicon.png">
+    <link rel="shortcut icon" href="<?php echo base_url().'assets/img/favicon_tie.ico'?>">
     
     <!-- Bootstrap 3.3.2 -->
     <link rel="stylesheet" href="<?php echo base_url().'assets/css/bootstrap.min.css'?>">
